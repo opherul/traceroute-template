e30c9b78e5c9fc85d1d75087d7e19a1be78a9ce5
opherul <opher.shur@nitzanim.tech>
2026-03-19 13:35:42 +0200

    Implement traceroute function stub
    Add a traceroute function stub using Scapy

---
+++ solution.py
@@ -0,0 +1,5 @@
+# Implement traceroute using Scapy
+# Your function must return a list of dicts with keys: hop, ip, rtt_ms
+
+def traceroute(dest, max_hops=30):
+    raise NotImplementedError("Implement this!")
